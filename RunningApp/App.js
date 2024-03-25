5cb81e81ed8a9cbda35e2be190fdada582d86946
ysatynska <ysatynska@mail.roanoke.edu>
2024-03-25 10:54:37 -0400

    bla

--- RunningApp/App.js
+++ RunningApp/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useRef} from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-// import { ChooseOptionPage } from './GoalScreen.js';
+import ChooseOptionPage from './GoalScreen.js';
 import SkillLevel from './SkillLevel.js';
 
 const Stack = createNativeStackNavigator();
@@ -9,7 +9,7 @@ export default function App () {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="goalScreen">
-        <Stack.Screen name="goalScreen" options={{ title: 'Enter Details' }} component={SkillLevel} />
+        <Stack.Screen name="goalScreen" options={{ title: 'Enter Details' }} component={ChooseOptionPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
