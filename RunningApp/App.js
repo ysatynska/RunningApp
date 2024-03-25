8fc04a3a17134288bc781d4b02ef15d01cca2dbc
ysatynska <ysatynska@mail.roanoke.edu>
2024-03-25 15:21:36 -0400

    added welcome screen

--- RunningApp/App.js
+++ RunningApp/App.js
@@ -2,15 +2,31 @@ import React, {useState, useEffect, useRef} from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import ChooseOptionPage from './GoalScreen.js';
+import Welcome from './Welcome.js';
 import SkillLevel from './SkillLevel.js';
+import {StyleSheet} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 export default function App () {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="goalScreen">
-        <Stack.Screen name="goalScreen" options={{ title: 'Enter Details' }} component={ChooseOptionPage} />
+      <Stack.Navigator initialRouteName="welcome">
+        <Stack.Screen name="welcome" options={{...styles.headerStyle, title: 'Welcome'}} component={Welcome} />
+        <Stack.Screen name="goalScreen" options={{...styles.headerStyle, title: 'What is Your Goal?'}} component={ChooseOptionPage} />
+        <Stack.Screen name="skillLevel" options={{...styles.headerStyle, title: 'Skill Level'}} component={SkillLevel} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    headerStyle: {
+      backgroundColor: '#01CFEE'
+    },
+    headerTintColor: 'white',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    }, 
+  }
+});
