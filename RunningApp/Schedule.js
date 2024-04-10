73d9d04c217cd24fd5f2756cbbf271d5f3a25170
Dylan Noell <dmnoell@mail.roanoke.edu>
2024-04-09 21:11:08 -0400

    1/2 finished with algorithm and set up user object

--- RunningApp/Schedule.js
+++ RunningApp/Schedule.js
@@ -1,47 +1,49 @@
-//import profile
+import user from "./userData"
 
-function generateDistanceSchedule(profile, schedule) {
+function generateDistanceSchedule(schedule) {
     let isTempo = false;
     let totalRuns = 0;
-     
-    if (profile.availability.monday.available === true) {
+    if (user.currentBest.distance < 1) {
+        user.currentBest.distance = 1;
+    }
 
-    }
-    if (profile.availability.tuesday.available === true) {
-        
-    }
-    if (profile.availability.wednesday.available === true) {
-        
-    }
-    if (profile.availability.thursday.available === true) {
-        
-    }
-    if (profile.availability.friday.available === true) {
-        
-    }
-    if (profile.availability.saturday.available === true && totalRuns < 5) {
-        
-    }
-    if (profile.availability.sunday.available === true && totalRuns < 5) {
-        
+    for (let i = 0; i < 7; i++) {
+        if (user.availability[i].available === true && totalRuns < 5) {
+            if (isTempo) {
+                schedule[i].distance = user.currentBest.distance/2;
+                schedule[i].reps = 3;
+                schedule[i].pace = 10;
+            }else{
+                schedule[i].distance = user.currentBest.distance*user.rateOfImprovement;
+                schedule[i].reps = 1;
+                user.currentBest.distance = user.currentBest.distance*user.rateOfImprovement;
+            }
+            isTempo = !isTempo;
+            totalRuns++;
+        }
     }
 }
 
-export default function generateSchedule(profile) {
-    const schedule = {
-        monday: {distance: 0, time: 0, reps: 0},
-        tuesday: {distance: 0, time: 0, reps: 0},
-        wednesday: {distance: 0, time: 0, reps: 0},
-        thursday: {distance: 0, time: 0, reps: 0},
-        friday: {distance: 0, time: 0, reps: 0}
-    };
+function generateTimeScedule(schedule) {
+    
+}
+
+export default function generateSchedule() {
+    let schedule = [
+        {day: "monday", distance: 0, pace: 0, reps: 0},
+        {day: "tuesday", distance: 0, pace: 0, reps: 0},
+        {day: "wednesday", distance: 0, pace: 0, reps: 0},
+        {day: "thursday", distance: 0, pace: 0, reps: 0},
+        {day: "friday", distance: 0, pace: 0, reps: 0},
+    ];
 
 
-    if (profile.goal.time === null){
-        schedule = generateDistanceSchedule(profile, schedule);
+    if (user.goal.time === 0){
+        generateDistanceSchedule(schedule);
     }else{
-        schedule = generateTimeScedule(profile, schedule);
+        generateTimeScedule(schedule);
     }
     
+    return schedule;
 
 }
