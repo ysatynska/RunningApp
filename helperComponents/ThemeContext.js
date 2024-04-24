c6fefe244b65d1739b58679b8f495bcc8e0af1ba
ysatynska <ysatynska@mail.roanoke.edu>
2024-04-23 21:16:15 -0400

    minor

--- helperComponents/ThemeContext.js
+++ helperComponents/ThemeContext.js
@@ -2,6 +2,7 @@
 //   different themes across the app
 import { createContext, useState, useContext } from 'react';
 import { themes } from './styles';
+import { useUser } from '../helperComponents/UserContext';
 
 const ThemeContext = createContext(); // Declare the context variable
 
@@ -11,12 +12,17 @@ export const useTheme = () => useContext(useTheme);
 
 // Define the ThemeProvider that will wrap all components using dynamic theme
 export const ThemeProvider = ({ children }) => {
+    const { user } = useUser();
     const [theme, setTheme] = useState(themes.light); // Default theme
 
     // Function to update theme when changed
     const toggleTheme = (themeName) => {
         setTheme(themes[themeName]);
     };
+
+    useEffect(() => {
+        setTheme(user.theme)
+    }, [user.theme]);
 
     // Wrap all components requiring access to the theme
     return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
